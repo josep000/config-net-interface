dea278f78774013da344c7a4dbb2e035cebde960
Jose Pinto <josep8686@gmail.com>
2022-09-11 12:37:54 -0300

    arreglos

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ import py2exe
 # setup.py py2exe
 
 setup(
-    name="ipconfig_v1.14",
+    name="ipconfig",
     version="1.0",
     description="Config ip address ipv4 easily an faster.",
     author="Jose Luis Pinto Hernandez",
     author_email="josep8686@gmail.com",
     url="joseluispinto.net",
     license="Compleatly Free License",
-    scripts=["ipconfig_v1.14.py"],
-    console=["ipconfig_v1.14.py"],
+    scripts=["ipconfig.py"],
+    console=["ipconfig.py"],
     options={"py2exe": {"bundle_files": 1}},
     zipfile=None,
 )
